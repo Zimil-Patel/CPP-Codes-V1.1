39b19dd8588a375d3e5829c2ba276941e6d492ec
Zimil-Patel <patelzimil2310@gmail.com>
2023-11-28 19:04:41 +0530

    2-11-2023

---
+++ Builder/SuperMarketSys.cpp
@@ -0,0 +1,14 @@
+/*
+Q.2 Write a Program to make a Supermarket Billing System.
+Requirements:
+(A) User Input:
+- Item Number
+- Item Name
+- Quantity
+- Price
+- Discount
+(B) Verify User Id And Password
+(C) Display Records:
+- All Records
+- By Searching Item Number
+*/
