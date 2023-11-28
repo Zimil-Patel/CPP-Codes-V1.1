41db5f88e18726897c6272540c4425899c66a522
Zimil-Patel <patelzimil2310@gmail.com>
2023-11-28 20:30:11 +0530

    28-11-2023

--- Builder/SuperMarketSys.cpp
+++ Builder/SuperMarketSys.cpp
@@ -31,7 +31,7 @@ class SuperMarket{
     
     public:
         //all setter methdos
-        void setItemData(int itemNumber = 0, string itemName, int itemQuantity = 0, int itemPrice = 0, int itemDiscount = 0){
+        void setItemData(int itemNumber, string itemName, int itemQuantity, int itemPrice, int itemDiscount){
 
             this->itemNumber = itemNumber;
             this->itemName = itemNumber;
@@ -46,12 +46,12 @@ class SuperMarket{
             return itemNumber;
         }
 
+        string getUserName(){
+            return userName;
+        }
 
-        SuperMarket(){
-            cout << endl << "- - - - - - - - - - - - - - - - - -" <<
-                    endl << "> Welcome to Super Market System <" <<
-                    endl << "- - - - - - - - - - - - - - - - - -" <<
-                    endl << endl;
+        string getPassword(){
+            return password;
         }
 
 };
@@ -65,7 +65,23 @@ class DashFunctions{
         SuperMarket items[3];
 
     public:
-        //show dashboard to user
+        //check id pass and login
+        int checkUser(string user, string pass){
+            if (user == items[0].getUserName() && pass == items[0].getPassword()){
+                cout << endl << ">> Login Successful <<" <<
+                        endl << endl;
+                system("pause");
+                system("cls");
+                return 1;
+            } else {
+                system("cls");
+                cout << endl << ">> Username or pass incorrect, try again! <<" <<
+                        endl << endl; 
+                return 0;
+            }
+        }
+
+        //show dashboard to user and get choice
         int showDash(){
 
             int choice;
@@ -84,6 +100,15 @@ class DashFunctions{
         }
 
 
+        //default constructor
+        DashFunctions(){
+            cout << endl << "- - - - - - - - - - - - - - - - - -" <<
+                    endl << "> Welcome to Super Market System <" <<
+                    endl << "- - - - - - - - - - - - - - - - - -" <<
+                    endl << endl;
+        }
+
+
 };
 
 //Assiging values to all static variable
@@ -95,6 +120,49 @@ string SuperMarket::password = "1234";
 int main(){
 
     DashFunctions perform;
+    int loginStatus = 0;
+    int choice = 0;
+    string user, pass;
+
+    //force user to login until getting correct username and pass
+    while(loginStatus == 0){
+
+        cout << endl << "Enter username : ";
+        getline(cin >> ws, user);
+
+        cout << "Enter password : ";
+        getline(cin >> ws, pass);
+
+        loginStatus = perform.checkUser(user, pass);
+    }
+
+    while(loginStatus){
+
+        choice = perform.showDash();
+
+        switch(choice){
+
+            case1 :
+            system("cls");
+            break;
+
+            case 2:
+            system("cls");
+            break;
+
+            case 3:
+            system("cls");
+            break;
+
+            case 4:
+            cout << endl << ">> Logout Successfull <<" <<
+                    endl << endl;
+            loginStatus = 0;
+            break;
+
+        }
+
+    }
 
     return 0;
 }
