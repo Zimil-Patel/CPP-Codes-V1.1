ce34cee3683c032e4534f3f00cd0c8d7e0f39e3d
Zimil-Patel <patelzimil2310@gmail.com>
2023-11-30 12:20:51 +0530

    30-11-2023

--- Encapsulation_3.3/HospitalSys.cpp
+++ Encapsulation_3.3/HospitalSys.cpp
@@ -24,6 +24,7 @@ class Hospital{
                     endl << endl;
         }
 
+        //set values
         void setter(string patientName, string disease, int age, long long mobileNo){
 
             this->patientName = patientName;
@@ -32,6 +33,7 @@ class Hospital{
             this->mobileNo = mobileNo;
         }
 
+        //display all records
         void getter(int count){
 
             cout << endl << ">> All Patient details are <<" <<
@@ -40,7 +42,7 @@ class Hospital{
             for (int i = 0; i < 3; i++){
 
                 cout << endl << ">>    Date : " << date <<
-                        endl << "      Time : " << appoinmentTime <<
+                        endl << "      Time : " << appoinmentTime + i << ":00 PM" <<
                         endl << "      Dotor : " << doctorName <<
                         endl << "      Patient Name : " << patientName <<
                         endl << "      Disease : " << disease <<
@@ -54,7 +56,38 @@ class Hospital{
 
 };
 
+
+string Hospital::date = "28-11-2023";
+string Hospital::doctorName = "Rajendra Shah.";
+int Hospital::appoinmentTime = 1;
+
 int main(){
+
+    string name, disease;
+    int age;
+    long long mobile;
+
+    Hospital data[3];
+
+    cout << endl << ">> Enter Patient details <<" <<
+            endl;
+
+    for (int i = 0; i < 3; i++){
+
+        cout << endl << ">>    Enter Patient " << i + 1 << 
+                endl;
+        cout << "Name : ";
+        getline(cin >> ws, name);
+        cout << "Disease : ";
+        getline(cin >> ws, disease);
+        cout << "Age : ";
+        cin >> age;
+        cout << "Mobile No. : ";
+        cin >> mobile;
+
+        data[i].setter(name, disease, age, mobile);
+
+    }
 
     return 0;
 }
