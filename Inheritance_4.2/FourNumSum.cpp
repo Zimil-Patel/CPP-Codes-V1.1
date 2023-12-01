dbeae131b13a15941ee59c3e11cb0311da7cb362
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-01 20:12:26 +0530

    01-12-2023

---
+++ Inheritance_4.2/FourNumSum.cpp
@@ -0,0 +1,95 @@
+/*
+Q.2 Write a Program to perform four different numbers’ sum by inheriting constructors in hybrid inheritance.
+*/
+
+#include<iostream>
+using namespace std;
+
+class A{
+
+    protected:
+        int num1;
+        int sum = 0;
+
+        void setNum1(){
+
+            cout << "Enter number 1 : ";
+            cin >> num1; 
+
+        }
+
+        void setSum(int num1, int num2, int num3, int num4){
+
+            sum = num1 + num2 + num3 + num4;
+
+        }  
+
+};
+
+class B : public A{
+
+    protected:
+        int num2;
+
+        void setNum2(){
+
+            cout << "Enter number 2 : ";
+            cin >> num2; 
+
+        }
+
+};
+
+class C : public A{
+
+    protected:
+        int num3;
+
+        void setNum3(){
+
+            cout << "Enter number 3 : ";
+            cin >> num3; 
+
+        }
+
+};
+
+
+class D : public B, C{
+
+    protected:
+        int num4;
+
+    public:
+        void setValues(){
+
+            B::setNum1();
+            setNum2();
+            setNum3();
+
+            cout << "Enter number 4 : ";
+            cin >> num4;
+
+            C::setSum(B::num1, num2, num3, num4);
+
+        };
+
+        void getSum(){
+
+            cout << endl << "SUM : " << C::sum <<
+                    endl << endl;
+
+        }
+
+};
+
+
+int main(){
+
+    D perform;
+
+    perform.setValues();
+    perform.getSum();
+
+    return 0;
+}
