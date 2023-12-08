80c51cc62eca2b92ad6345630c7f66475afa3811
Zimil Patel <112332000+Zimil-Patel@users.noreply.github.com>
2023-12-08 16:29:01 +0530

    KmMeter.cpp

--- Multiprocessor/KmMeter.cpp
+++ Multiprocessor/KmMeter.cpp
@@ -1,3 +1,14 @@
+/*
+Q.2 Write a Program to add two distances using binary plus (+) operator overloading.
+
+For example,
+Input:
+input1 => Km: 3, Meter: 1020
+input2 => Km: 2, Meter: 2050
+
+Output:
+Km: 8, Meter: 70
+*/
 #include<iostream>
 using namespace std;
 
