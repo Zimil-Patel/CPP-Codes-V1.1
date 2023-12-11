6d4fec7b8c30464d4766510163048b89baf39b4c
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-11 18:49:01 +0530

    11-12-2023

---
+++ Abstraction_6.1/AdminEmpManager.cpp
@@ -0,0 +1,87 @@
+/*
+Q.1 Write a Program to abstract some attributes of class Admin to prevent them to be inherited by its child classes.
+- Class Admin -> Class Manager
+- Class Manager -> Class Employee
+- Class Admin has the following members:
+company_name
+manager_salary
+employee_salary
+total_staff
+total_annual_revenue
+can_terminate
+- use all attributes accessibility in the parent class and child class properly.
+- override a method myAccess() in both child classes to display all parent class members' values accordingly.
+*/
+
+#include<iostream>
+using namespace std;
+
+class Admin{
+
+    protected:
+        string company_name = "LNT";
+        int manager_salary = 80000;
+        int employee_salary = 50000;
+        int total_staff = 20;
+        int total_annual_revenue = 1.57;
+        bool can_terminate = true;
+
+    public:
+        void myAccess(){
+
+            cout << endl << ">>  By Admin Access : " << endl;
+            cout << "    Company Name : " << company_name << endl
+                 << "    Manager Salary : " << manager_salary << endl
+                 << "    Employee Salary : " << employee_salary << endl
+                 << "    Total Staff : " << total_staff << endl
+                 << "    Total Annual Revenue : " << total_annual_revenue << endl
+                 << "    Can Terminate : " << can_terminate << endl; 
+
+        }
+
+};
+
+class Manager : public Admin{
+
+    public:
+        void myAccess(){
+
+            cout << endl << ">>  By Manager Access : " << endl;
+            cout << "    Company Name : " << company_name << endl
+                 << "    Manager Salary : " << manager_salary << endl
+                 << "    Employee Salary : " << employee_salary << endl
+                 << "    Total Staff : " << total_staff << endl
+                 << "    Total Annual Revenue : " << total_annual_revenue << endl;
+
+        }
+
+};
+
+class Employee : public Admin{
+
+    public:
+        void myAccess(){
+
+            cout << endl << ">>  By Employee Access : " << endl;
+            cout << "    Company Name : " << company_name << endl
+                 << "    Employee Salary : " << employee_salary << endl
+                 << "    Total Annual Revenue : " << total_annual_revenue << endl;
+
+        }
+
+};
+
+
+
+int main(){
+
+    Admin admin;
+    Manager manager;
+    Employee employee;
+
+    admin.myAccess();
+    manager.myAccess();
+    employee.myAccess();
+
+    return 0;
+}
