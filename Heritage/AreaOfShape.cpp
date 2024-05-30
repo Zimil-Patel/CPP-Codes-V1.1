f75a2a10802439c028f6e0583aa8a2ac5ea9295d
Zimil Patel <112332000+Zimil-Patel@users.noreply.github.com>
2024-05-30 20:01:21 +0530

    AreaOfShape.cpp

--- Heritage/AreaOfShape.cpp
+++ Heritage/AreaOfShape.cpp
@@ -3,7 +3,6 @@ Q.1 Write a Program that defines a shape class with a constructor that gives val
 Then define two sub-classes triangle and rectangle, that calculate the area of the shape area ().
 In the main, define two objects a triangle and a rectangle, and then call the area() function using these two objects.
 */
-
 #include<iostream>
 using namespace std;
 
